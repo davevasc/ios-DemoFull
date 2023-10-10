586377eb9be1fb74fd57ae061f2e8f6c93a3b70d
dave <daveplus@gmail.com>
2023-10-10 18:52:57 +0200

    primer commit

--- DemoFull/ContentView.swift
+++ DemoFull/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // primer comentario
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
