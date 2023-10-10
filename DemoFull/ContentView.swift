6a486e82f48acdcf4b276ea0d5418edc2072fa92
dvicente <dvicente@imbee.me>
2023-10-11 00:05:20 +0200

    commit de segundo comentario desde sourcetree

--- DemoFull/ContentView.swift
+++ DemoFull/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             // primer comentario
+            // segundo comentario commit desde sourcetree
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
