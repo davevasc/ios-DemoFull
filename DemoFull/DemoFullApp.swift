007d466ddd11eb7a29d578b91bf5a7e1125770a8
dvicente <dvicente@imbee.me>
2023-10-10 18:09:59 +0200

    Initial Commit

---
+++ DemoFull/DemoFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFullApp.swift
+//  DemoFull
+//
+//  Created by David Vicente on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
